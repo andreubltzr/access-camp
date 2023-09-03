7707be66312771af255e10b57937ac08487dd40a
andreubltzr <andreubltzr11@gmail.com>
2023-09-03 18:37:41 +0200

    removed unused imports

--- src/main/java/com/example/springproject/activity/ActivityService.java
+++ src/main/java/com/example/springproject/activity/ActivityService.java
@@ -1,8 +1,6 @@
 package com.example.springproject.activity;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
